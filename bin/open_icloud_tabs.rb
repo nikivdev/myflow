15b17076b43032da32a0ffd495f0a2726002faaf
Nikita Voloboev <nikita.voloboev@gmail.com>
2020-10-17 21:29:55 +0200

    open_icloud_tabs karabiner settings

--- bin/open_icloud_tabs.rb
+++ bin/open_icloud_tabs.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/ruby
 
-# CloudTabOpener 
+# CloudTabOpener
 #
 #   written by: David Blache
 #   free to modify and use however you see fit  : )
 #
-# Queries the ~/Library/Safari/CloudTabs.db database, letting you select and open iCloud 
+# Queries the ~/Library/Safari/CloudTabs.db database, letting you select and open iCloud
 # tabs associated with any of your Apple devices
 
 
@@ -21,38 +21,38 @@ class CloudTabOpener
 
   DBPATH = "~/Library/Safari/CloudTabs.db"
   #DBPATH = "~/Desktop/CloudTabs.db"
-  
-  
+
+
   def initialize
     @database = File.expand_path(DBPATH)
     load_gem("sqlite3")
   end
-  
-  
+
+
   def start
     # ask user to choose device from iCLoud tab device list
     device = choose_device
     unless device.nil?
-    
+
       # get iCloud tabs for this device
       tabs = get_tabs_for_device(device)
-    
+
       # are you sure?
       should_open = true
-      if tabs.count > 10 
+      if tabs.count > 10
         puts "There are #{tabs.count} tabs open on #{device[:name]}."
         print "> Open #{tabs.count} tabs?: "
         choice = STDIN.gets.chomp.downcase
         should_open = false unless ["yes", "y"].include? choice
       end
-    
+
       # open them in a new window
       if should_open
         tabs.each_with_index do |tab, index|
           if index == 0
-            command = "osascript -e 'tell application \"Safari\" to make new document with properties {URL: \"#{tab[:url]}\"}' > /dev/null"
+            command = "osascript -e 'tell application \"Safari Technology Preview\" to make new document with properties {URL: \"#{tab[:url]}\"}' > /dev/null"
           else
-            command = "osascript -e 'tell application \"Safari\" to tell window 1 to make new tab with properties {URL: \"#{tab[:url]}\"}' > /dev/null"
+            command = "osascript -e 'tell application \"Safari Technology Preview\" to tell window 1 to make new tab with properties {URL: \"#{tab[:url]}\"}' > /dev/null"
           end
           puts "Opening #{index + 1} of #{tabs.count}: #{tab[:title]}"
           system(command)
@@ -60,11 +60,11 @@ class CloudTabOpener
       end
     end
   end
-  
-  
+
+
   def choose_device()
     selected_device = nil
-    
+
     devices = get_devices
     if devices.count == 1
       selected_device = devices[0]
@@ -72,7 +72,7 @@ class CloudTabOpener
       if devices.count > 0
         puts "Choose the device whose iCloud tabs you want to open."
         devices.each_with_index {|device, i| puts "#{i}: #{device[:name].strip}"}
-  
+
         asking = true
         while asking
           print "> Device: "
@@ -93,11 +93,11 @@ class CloudTabOpener
         puts "Selected: #{selected_device[:name]}"
       end
     end
-    
+
     return selected_device
   end
-  
-  
+
+
   def get_devices()
     devices = Array.new
     begin
@@ -107,7 +107,7 @@ class CloudTabOpener
       results.each do |row|
         devices << {id: row[0], name:row[1]}
       end
-    rescue SQLite3::Exception => e 
+    rescue SQLite3::Exception => e
       STDERR.puts "ERROR: #{__method__}: #{e}"
     ensure
       statement.close if statement
@@ -115,8 +115,8 @@ class CloudTabOpener
     end
     return devices
   end
-  
-  
+
+
   def get_tabs_for_device(device)
     tabs = Array.new
     begin
@@ -126,7 +126,7 @@ class CloudTabOpener
       results.each do |row|
         tabs << {title: row[0], url:row[1]}
       end
-    rescue SQLite3::Exception => e 
+    rescue SQLite3::Exception => e
       STDERR.puts "ERROR: #{__method__}: #{e}"
     ensure
       statement.close if statement
@@ -134,8 +134,8 @@ class CloudTabOpener
     end
     return tabs
   end
-  
-    
+
+
   def load_gem(name, version = nil)
     begin
       gem name, version
@@ -146,7 +146,7 @@ class CloudTabOpener
     end
     require name
   end
-  
+
 end
 
 opener = CloudTabOpener.new()
