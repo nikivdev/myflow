c6d8700ffbeb0739a4378aa322e3eaf5d688e10d
Nikita Voloboev <nikita.voloboev@gmail.com>
2021-11-21 21:50:42 +0100

    karabiner

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,3 +1,8 @@
+
+#### FIG ENV VARIABLES ####
+# Please make sure this block is at the start of this file.
+[ -s ~/.fig/shell/pre.sh ] && source ~/.fig/shell/pre.sh
+#### END FIG ENV VARIABLES ####
 source ~/.zsh_plugins.sh # Load zsh plugins (using antibody)
 source ~/.dotfiles/zsh/env.zsh
 source ~/.dotfiles/zsh/options.zsh
@@ -14,3 +19,8 @@ export PATH="$BUN_INSTALL/bin:$PATH"
 
 # Bun completions
 [ -s "/Users/nikivi/.bun/_bun" ] && source "/Users/nikivi/.bun/_bun"
+
+#### FIG ENV VARIABLES ####
+# Please make sure this block is at the end of this file.
+[ -s ~/.fig/fig.sh ] && source ~/.fig/fig.sh
+#### END FIG ENV VARIABLES ####
