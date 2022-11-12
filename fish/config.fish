8d7c8e52762b81cb8091982b868fb9f5f4ffbd12
Nikita Voloboev <nikita.voloboev@gmail.com>
2022-11-12 01:33:54 +0100

    .

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,15 @@
 # testing
 
 # path
-fish_add_path /opt/homebrew/bin
-fish_add_path ~/.dotfiles/bin
-fish_add_path $HOME/.cargo/bin # rust
+# fish_add_path /opt/homebrew/bin
+# fish_add_path ~/.dotfiles/bin
+# fish_add_path $HOME/.cargo/bin # rust
 # set -x PATH "$HOME/.cargo/bin:$PATH"
 
 # source
 source ~/.dotfiles/fish/alias.fish
 source ~/.dotfiles/fish/functions.fish
-source "$HOME/.cargo/env"
+# source "$HOME/.cargo/env"
 
 __fish_cursor_xterm line # make cursor into thin line (doesn't make a difference in Warp.dev)
 
