2abb5dc121077e8cd43a7607c067de159c60b337
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-01-21 20:28:48 +0100

    .

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,22 @@
 # testing
 
 # path
-
 fish_add_path ~/.bun/bin/
 fish_add_path ~/src/config/bin
 fish_add_path ~/src/config/bin/private
-# fish_add_path /opt/homebrew/bin
-# fish_add_path ~/.dotfiles/bin
-# fish_add_path $HOME/.cargo/bin # rust
-# set -x PATH "$HOME/.cargo/bin:$PATH"
 
 # source
 source ~/src/config//fish/alias.fish
 source ~/src/config/fish/functions.fish
-# source "$HOME/.cargo/env"
-
-__fish_cursor_xterm line # make cursor into thin line (doesn't make a difference in Warp.dev)
 
 # pnpm
 set -gx PNPM_HOME /Users/nikiv/Library/pnpm
 set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
 
 # go
 set -x GOPATH (go env GOPATH)
 set -x PATH $PATH (go env GOPATH)/bin
 set -x GOKU_EDN_CONFIG_FILE ~/src/config/karabiner/karabiner.edn
+
+# other
+__fish_cursor_xterm line # make cursor into thin line (doesn't make a difference in Warp.dev)
