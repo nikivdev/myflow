b6acba9ef5e4e2e2c05fca797cd4acc7a923122d
Evan Liu <evancoding@gmail.com>
2024-06-02 10:21:41 +1200

    Add simlayer s

--- karabiner/karabiner-config/src/index.ts
+++ karabiner/karabiner-config/src/index.ts
@@ -1,4 +1,4 @@
-import { FromKeyParam, map, simlayer, to$ as $, ToKeyParam, withMapper, writeToProfile } from "karabiner.ts"
+import { FromKeyParam, map, rule, simlayer, to$ as $, toKey, ToKeyParam, withMapper, withModifier, writeToProfile } from "karabiner.ts"
 
 writeToProfile(
   "--dry-run", // prints to console
@@ -17,7 +17,35 @@ writeToProfile(
     ]),
 
     simlayer("s").description('skey (essential)').manipulators([
-      map('w').to('←', '⌥').to('→', '⌥⇧') // Highlight word
+      map('w').to('←', '⌥').to('→', '⌥⇧'), // Highlight word
+      map('quote').to('←', '⌘').to('→', '⌘⇧'), // Highlight current line
+
+      // Or map('e').to('⇥'),
+      {
+        e: toKey('tab'),
+        r: toKey('tab', '⇧'),
+
+        a: toKey('c', '⌘'), // Copy
+        n: toKey('v', '⌘'), // Paste
+        o: toKey('x', '⌘'), // Cut
+      },
+
+      // Or map('h', '??').to('←'),
+      withModifier('optionalAny')({
+        h: toKey('←'),
+        j: toKey('↓'),
+        k: toKey('↑'),
+        l: toKey('→'),
+
+        b: toKey('←', '⌘'),
+        m: toKey('→', '⌘'),
+      }),
+    ]),
+
+    rule('swap : and ;').manipulators([
+      // Or map(';', '?⇪').to(';', '⇧'),
+      map('semicolon', {optional: 'caps_lock'}).to('semicolon', 'shift'),
+      map('semicolon', 'shift').to('semicolon')
     ]),
 
     simlayer("w").manipulators({
@@ -28,6 +56,7 @@ writeToProfile(
     "basic.to_if_alone_timeout_milliseconds": 80,
     "basic.to_if_held_down_threshold_milliseconds": 50,
     "basic.to_delayed_action_delay_milliseconds": 0,
+    'basic.simultaneous_threshold_milliseconds': 30,
     "simlayer.threshold_milliseconds": 250,
   }
 )
