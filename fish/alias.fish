1116b9dd59574dc30d5d427aa8d9752c5dad3e54
Nikita <nikita.voloboev@gmail.com>
2024-07-04 21:38:14 +0200

    .

--- fish/alias.fish
+++ fish/alias.fish
@@ -11,6 +11,7 @@ alias dwi="cd ~/data/wiki"
 alias d.="cd ~/.scripts"
 alias db="cd ~/bin"
 alias dw="cd ~/Downloads"
+alias do="cd ~/src/org"
 
 # rust
 # alias rbnowarning="RUSTFLAGS="-A warnings" cargo build"
