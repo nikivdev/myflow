f3d46b744851fdf0b5ac708dabb97028c640af12
Nikita <github@nikiv.dev>
2024-07-29 18:59:19 +0400

    .

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ source ~/src/config/fish/alias_and_functions.fish
 # direnv (cd into folder, will get the `.env` etc.)
 direnv hook fish | source
 
-# activates global flox env
-# if set -q SHLVL
-#   eval $(flox activate -d $HOME)
-# end
+# activates global flox env. TODO: slows down shell by a bit, not sure why and how to improve
+if set -q SHLVL
+  eval $(flox activate -d $HOME)
+end
--- fish/past_alias_and_functions.fish
+++ fish/past_alias_and_functions.fish
@@ -1,3 +1,56 @@
+# cd places
+alias dg="cd ~/src/config"
+alias de="cd ~/Desktop"
+alias ds="cd ~/try"
+alias dm="cd ~/temp"
+alias dv="cd ~/try/private"
+alias dt="cd ~/data"
+alias dwi="cd ~/data/wiki"
+alias d.="cd ~/.scripts"
+alias db="cd ~/bin"
+alias dw="cd ~/Downloads"
+alias do="cd ~/src/org"
+# rust
+# alias rbnowarning="RUSTFLAGS="-A warnings" cargo build"
+# run code fast
+alias :ts="fish -c 'cd ~/src/ts; bun run try'"
+# other
+alias :id="bun i -d" # dev dep install
+# alias :id="pnpm add -d" # dev dep install
+alias a="eza" # list files
+alias aa="eza -la" # list files (with hidden)
+alias cwd='pwd | pbcopy'
+alias v="mv" # move files/folders or rename
+alias sf="source ~/.config/fish/config.fish" # source fish config
+alias rr="rm -rf"
+alias md="mkdir"
+alias wgi="watchexec --restart --exts go \"tput reset && go install .\""
+alias wg="watchexec --restart --exts go \"tput reset && go run .\""
+# alias dw="deno task watch"
+alias :d="bun dev"
+alias :t="bun test --watch"
+alias e="edgedb"
+alias :s="cd ~/src/ts/packages/solid && bun dev" # run solid web server
+alias :j="cd ~/src/ts && bun dev"
+# alias :r="cd ~/src/rust && bun dev"
+alias :t="cd ~/src/python && bun dev"
+alias getLastCommitHash="git log -1 --format=\"%H\""
+# alias pr="pnpm run"
+alias w.="cursor .env"
+# alias pw='pnpm add -w'
+alias gmt='go mod tidy'
+alias findAll.gitInCurrentDir="find . -type d -name .git"
+alias :g="bun install -g"
+alias nv="sudo nvim"
+# alias .="open ."
+alias o="ollama"
+alias z="zig"
+alias gl="git pull"
+alias python="python3"
+alias s="swift"
+alias wr="cursor readme.md"
+alias dr="cd ~/private"
+
 # from https://gist.github.com/nikoheikkila/dd4357a178c8679411566ba2ca280fcc
 function envsource
     for line in (cat $argv | grep -v '^#' | grep -v '^\s*$')
