e854fa0eb7332244006df449b2ff1bcd3e37773c
Nikita <github@nikiv.dev>
2024-08-20 23:16:18 +0300

    .

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ fish_add_path ~/.cargo/bin
 source ~/src/config/fish/alias-and-functions.fish
 source ~/src/config/fish/private.fish
 
-source ~/.local/state/nix/profiles/profile-1-link/etc/profile.d/nix.fish # use latest version of nix
+source ~/.local/state/nix/profiles/profile/etc/profile.d/nix.fish # use latest version of nix
 set -x NIX_SSL_CERT_FILE /etc/ssl/certs/ca-certificates.crt # needed for flox/nix (some ssl bug or something)
 # TODO: how to use latest version of nix daemon too with flox?
 
@@ -17,9 +17,9 @@ set -x DIRENV_LOG_FORMAT "" # hide direnv unloading messages
 # activates global flox env
 # TODO: disabled now as it slows down shell massively (mostly due to network call) (what flox is doing: https://gist.github.com/nikitavoloboev/4435ba3afa291bda38f9373aa1a3adef)
 # if set -q SHLVL
-#   _date +%T.%N >> ~/log/config.fish
+#   # _date +%T.%N >> ~/log/config.fish
 #   # eval $(flox activate -d $HOME -vvvvv)
 #   eval $(flox activate -d $HOME)
-#   _date +%T.%N >> ~/log/config.fish
-#   echo ">>>>>>>>>>" >> ~/log/config.fish
+#   # _date +%T.%N >> ~/log/config.fish
+#   # echo ">>>>>>>>>>" >> ~/log/config.fish
 # end
