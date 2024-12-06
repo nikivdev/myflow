b7b6925ef7b17e079ca9e9fe27274528698eb938
Nikita <github@nikiv.dev>
2024-12-06 23:22:16 +0100

    ..

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,8 @@ source ~/.local/state/nix/profiles/profile/etc/profile.d/nix.fish # use latest v
 set -x NIX_SSL_CERT_FILE /etc/ssl/certs/ca-certificates.crt # needed for flox/nix (some ssl bug or something) TODO: still needed?
 # TODO: how to use latest version of nix daemon too with flox?
 
-direnv hook fish | source # direnv (cd into folder, will get the `.env` etc.)
-set -x DIRENV_LOG_FORMAT "" # hide direnv unloading messages
+# direnv hook fish | source # direnv (cd into folder, will get the `.env` etc.)
+# set -x DIRENV_LOG_FORMAT "" # hide direnv unloading messages
 
 fish_add_path $HOME/.flox/run/aarch64-darwin.default.dev/bin
 fish_add_path ~/go/bin
@@ -22,3 +22,15 @@ fish_add_path /Users/nikiv/.codeium/windsurf/bin
 
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /Users/nikiv/.cache/lm-studio/bin
+
+# fnm
+# TODO: lookgs ugly and seems wrong to commit this in config.fish, fix it
+set -gx PATH "/Users/nikiv/.local/state/fnm_multishells/6982_1733503676213/bin" $PATH;
+set -gx FNM_MULTISHELL_PATH "/Users/nikiv/.local/state/fnm_multishells/6982_1733503676213";
+set -gx FNM_VERSION_FILE_STRATEGY "local";
+set -gx FNM_DIR "/Users/nikiv/Library/Application Support/fnm";
+set -gx FNM_LOGLEVEL "info";
+set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist";
+set -gx FNM_COREPACK_ENABLED "false";
+set -gx FNM_RESOLVE_ENGINES "true";
+set -gx FNM_ARCH "arm64";
